a4a8be39f9e02c2ee1473a6c9546126cb34ecad4
KenM4sters <EHCKenway@outlook.com>
2023-12-23 07:33:21 +0000

    specular lighting - beta

--- src/frame_info.hpp
+++ src/frame_info.hpp
@@ -13,11 +13,13 @@ namespace lve {
     struct PointLight {
         glm::vec4 position{};
         glm::vec4 color{};
+
     };
 
     struct GlobalUbo {
         glm::mat4 projection{1.0f};
         glm::mat4 view{1.0f};
+        glm::mat4 inverseView{1.0f};
         glm::vec4 ambientColor{1.0f, 1.0f, 1.0f, 0.02f};
         PointLight pointLights[MAX_LIGHTS];
         int numLights;
